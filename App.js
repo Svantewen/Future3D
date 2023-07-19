d0046925b67e1477172f78dfb13ec7e7117393c5
Svante Wenander <svante.wenander@gmail.com>
2023-07-19 19:28:51 +0200

    background of Berlin

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const PlaceholderImage = require('./assets/images/berlin.jpg');
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={PlaceholderImage} style={styles.image} />
+      </View>
       <Text style={{ color: 'blue' }}>Svanten text</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +18,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
+
+
+
+
